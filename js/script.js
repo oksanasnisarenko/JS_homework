a265cc5a02debe62a28485a7a91b9d75de3ed8f4
Oksana <oksanasnisarenko05@gmail.com>
2024-01-03 21:32:30 +0100

    lesson27_commit

--- js/script.js
+++ js/script.js
@@ -1,93 +1,77 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-	}
-}
-start(); 
 
 const personalMovieDB = {
-	count: numberOfFilms,
+	count: 0,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
-};
-
-
-function rememberMyFilms () {
-	for (let i = 0; i < 2; i++) {
-		const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', '');	 
-		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('done');
-		} else {
-			console.log('error');
-			i--;
-		}	
-	}
-}
-rememberMyFilms ();
-
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log('Просмотрено довольно мало фильмов');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		console.log('Вы классический зритель');
-	} else if (personalMovieDB.count >= 30) {
-		console.log('Вы киноман');
-	} else {
-		console.log('Произошла ошибка');
-	}
-}
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-	if (!hidden) {
-		console.log(personalMovieDB);
-	}
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-	}
-}
-
-writeYourGenres();
-
-
-
-
-function calculateVolumeAndArea(length) {
-	if (typeof(length) !== 'number' || length < 0 || !Number.isInteger(length)) {
-		return 'При вычислении произошла ошибка';
-	}
-	let volume = 0,
-		area = 0;
+	privat: false,
+	start: function() {
+		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 	
-	volume = length * length * length;
-	area = 6 * (length * length);
-	return `Обьем куба: ${volume}, площа всей поверхности: ${area}`;
-}
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+		}
+	},
 
-calculateVolumeAndArea(3);
+	rememberMyFilms: function() {
+		for (let i = 0; i < 2; i++) {
+			const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');	 
+			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+				personalMovieDB.movies[a] = b;
+				console.log('done');
+			} else {
+				console.log('error');
+				i--;
+			}	
+		}
+	},
 
-function getCoupeNumber(place) {
-	if (typeof(place) !== 'number' || place < 0 || !Number.isInteger(place)) {
-		return console.log('Ошибка. Проверьте правильность введенного номера места');
-	} else if (place === 0 || place > 36) {
-		return console.log('Таких мест в вагоне не существует');
-	} else {
-		return console.log(Math.ceil(place / 4));
+	detectPersonalLevel: function() {
+		if (personalMovieDB.count < 10) {
+			console.log('Просмотрено довольно мало фильмов');
+		} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+			console.log('Вы классический зритель');
+		} else if (personalMovieDB.count >= 30) {
+			console.log('Вы киноман');
+		} else {
+			console.log('Произошла ошибка');
+		}
+	},
+
+
+	showMyDB: function(hidden) {
+		if (!hidden) {
+			console.log(personalMovieDB);
+		}
+	},
+
+	toggleVisibleDB: function() {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
+		}else {
+			personalMovieDB.privat = true;
+		}
+
+	},
+
+
+	writeYourGenres: function() {
+		for (let i = 1; i <= 3; i++) {
+
+			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+			if (genre === '' || genre == null) {
+				console.log('Вы ввели некорректные данные или не ввели их вовсе');
+				i--;
+			} else {
+				personalMovieDB.genres[i - 1] = genre;
+			}
+		}
+		personalMovieDB.genres.forEach((item, i) => {
+			console.log(`Любимый жанр ${i + 1} - это ${item}`);
+		});
 	}
 
-}
-getCoupeNumber(33);
+};
